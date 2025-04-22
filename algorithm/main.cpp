dfb2d539318daf877cadf85d6be3d8ecb9316e73
antllcon <pokemonivan32@gmail.com>
2025-04-22 22:35:03 +0300

    Добавлен файл инициализации работы приложения для тестирования класса Graph

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -1,26 +1,60 @@
-#include "class-app/App.h"
+#include "class-app/app.h"
+#include "class-graph/graph.h"
 #include "config.h"
 #include <cstdlib>
 #include <iostream>
+
+// Алгоритм Гхоша для поиска охранников
+Guards GetGuards(const Graph& graph)
+{
+	Guards guards;
+	// Проведение линий через каждую пару вершин многоугольника
+	// и вычисление всех компонент видимости
+	auto viewComponents = graph.GetViewComponents();
+
+	// TODO: Подумать как работать, стоит ли передавать сами объекты, а не их номера
+
+	// Получение множества веерных многоугольников
+	// добавляя те компоненты видимости, которые полностью видны из текущей вершины
+	// auto funGrpahs = graph.GetFunGraphs(viewComponents);
+
+	// Жадный алгоритм покрытия для поиска вершин
+	// auto guards = graph.GetGuards(funGrpahs);
+
+	// Получение координат вершин охранников
+	// (возмжоно это не нужно, но на всякий случай)
+
+	return guards;
+}
 
 int main()
 {
 	try
 	{
-		sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Pixel Poem");
+		// Инициализация приложения
+		sf::RenderWindow window(sf::VideoMode(screen::WIDTH, screen::HEIGHT), "Pixel Poem");
 		App app;
 
+		// Создание графа из файла
+		FileToGraphAdapter adapter;
+		Graph artGallery = adapter.ConvertEdgeListToMatrix("data.txt");
+
+		// Вывод графа
+		std::cout << artGallery << std::endl;
+
+		// Цикл приложения
 		while (window.isOpen())
 		{
-			app.ProcessEvents(window);
-			app.Render(window);
+			app.ProcessEvents(window, artGallery);
+			app.Render(window, artGallery);
 		}
 	}
 	catch (const std::exception& errorMessage)
 	{
-		std::cerr << errorMessage.what() << '\n';
+		std::cerr << "Ошибка: " << errorMessage.what() << std::endl;
 		return EXIT_FAILURE;
 	}
 
+	std::cout << "Программа завершена успешно" << std::endl;
 	return EXIT_SUCCESS;
 }
