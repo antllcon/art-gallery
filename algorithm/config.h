f14eb1a7cad4d16f29bdfcc17db1022b434c92db
antllcon <pokemonivan32@gmail.com>
2025-04-22 22:33:14 +0300

    Добавлен файл конфигурации проекта (набор константных значений)

--- algorithm/config.h
+++ algorithm/config.h
@@ -1,22 +1,37 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#include <SFML/Graphics.hpp>
 
 // === Настройки экрана ===
-constexpr size_t SCREEN_WIDTH = 800;
-constexpr size_t SCREEN_HEIGHT = 600;
+namespace screen
+{
+constexpr size_t WIDTH = 800;
+constexpr size_t HEIGHT = 600;
+} // namespace screen
 
 // === Цветовые настройки ===
-const sf::Color COLOR_BLACK = sf::Color(33, 33, 33);
-const sf::Color COLOR_DARK = sf::Color(51, 51, 51);
-const sf::Color COLOR_GRAY = sf::Color(69, 69, 69);
-const sf::Color COLOR_WHITE = sf::Color(255, 255, 255);
-const sf::Color COLOR_TRANSPARENT = sf::Color(0, 0, 0, 0);
+namespace color
+{
+const sf::Color BLACK = sf::Color(33, 33, 33);
+const sf::Color DARK = sf::Color(51, 51, 51);
+const sf::Color GRAY = sf::Color(69, 69, 69);
+const sf::Color WHITE = sf::Color(255, 255, 255);
+const sf::Color TRANSPARENT = sf::Color(0, 0, 0, 0);
 
-const sf::Color COLOR_PINK = sf::Color(255, 0, 127);
-const sf::Color COLOR_RED = sf::Color(153, 0, 0);
-const sf::Color COLOR_YELLOW = sf::Color(255, 255, 200);
-const sf::Color COLOR_GREEN = sf::Color(102, 153, 102);
-const sf::Color COLOR_BLUE = sf::Color(100, 0, 255);
-const sf::Color COLOR_PURPLE = sf::Color(204, 0, 102);
+const sf::Color PINK = sf::Color(255, 0, 127);
+const sf::Color RED = sf::Color(153, 0, 0);
+const sf::Color YELLOW = sf::Color(255, 255, 200);
+const sf::Color GREEN = sf::Color(102, 153, 102);
+const sf::Color BLUE = sf::Color(100, 0, 255);
+const sf::Color PURPLE = sf::Color(204, 0, 102);
+
+} // namespace color
+
+// === Существование ребра ===
+namespace constants
+{
+inline constexpr size_t EXIST = 1;
+inline constexpr size_t EMPTY = 0;
+} // namespace constants
 
 #endif
